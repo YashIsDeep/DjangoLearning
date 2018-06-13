b8a104a60c273b7d6f2abdacdf3c17b880082b1d
YashIsDeep <yashparth.gupta@gmail.com>
2018-06-13 16:49:23 +0530

    Completed till Video 25

--- music/models.py
+++ music/models.py
@@ -14,6 +14,7 @@ class Song(models.Model):
 	album = models.ForeignKey(Album ,on_delete = models.CASCADE)
 	file_type=models.CharField(max_length=100)
 	title=models.CharField(max_length=100)
+	is_favorite=models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.title
--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,15 @@
 
 from django.urls import include,path
 from . import views
+
+app_name='music'
 urlpatterns = [
     # /music/
     path('',views.index,name="index"),
 
     # /music/1234/
-    path('<int:album_id>/',views.detail,name ='detail')
+    path('<int:album_id>/',views.detail,name ='detail'),
+
+    # /music/1234/favorite/
+    path('<int:album_id>/favorite/',views.favorite,name ='favorite'),
 ]
